4e57cf39d8b478e2225f901a1661cab78af637db
adeeba reaz <adeebarivera@outlook.com>
2020-04-29 22:34:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,3 +13,20 @@
 
 # end
 
+class Shoe
+  attr_accessor :color, :size, :material, :condition	  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand	  attr_reader :brand
+
+  BRANDS = []
+
+
+  def initialize(brand)	  
+    @brand = brand	    
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
+  end	
+
+
+
+
+  def cobble	  def cobble
+    self.condition = "new"	    self.condition = "new"
